ffa54293fb415bd1629b1e15d5f7a86f3df05411
Botshelo Tlhabanyane <79863670+BotsheloT@users.noreply.github.com>
2022-05-03 11:30:54 +0200

    small changes

--- Basics/sandbox.js
+++ Basics/sandbox.js
@@ -1,3 +1,4 @@
 var area = function (length) { return length * length; };
 console.log(area(5));
 var cArea = function (radius) { return Math.PI * (Math.pow(radius, 2)); };
+var sArea = function (l, b, h) { return l * b * h; };
--- Basics/sandbox.ts
+++ Basics/sandbox.ts
@@ -1,3 +1,6 @@
 let area = (length: number) => {return length*length};
 console.log(area(5));
+
 let cArea = (radius: number) => {return Math.PI*(radius**2)};
+
+var sArea = function (l, b, h) {return l * b *h};
